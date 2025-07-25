c67795f9f4896842ee42029d98081fd98524d06a
Ruchita Bachhav <ruchita@companyhub.com>
2025-07-25 11:21:35 +0530

    Add Actions/Subscriber is updated/index.js

---
+++ Actions/Subscriber is updated/index.js
@@ -0,0 +1,19 @@
+var action={
+    subscribe: function (request) {
+        var body = {};
+        body.url = request.TargetUrl;
+        body.event = "subscriber.update";
+        var subscribeResponse = executeAction('Create Webhook', JSON.stringify(body));
+        var subscribeBody = JSON.parse(subscribeResponse);
+        return {
+            Success: true,
+            Data: JSON.stringify({
+                WebhookId: subscribeBody.id,
+            })
+        };
+    },
+    unsubscribe: function (request) {
+        var unsubscribeResponse = executeAction('Delete webhook', JSON.stringify({ id: JSON.parse(request.SubscriptionData).WebhookId }));
+        return { Success: true };
+    }
+};
