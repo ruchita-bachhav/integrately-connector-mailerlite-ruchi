f0513ff7aa058c5d6593ad1845c0d87b81df61fc
Ruchita Bachhav <ruchita@companyhub.com>
2025-07-25 11:21:58 +0530

    Add Actions/Unsubscribe subscriber/index.js

---
+++ Actions/Unsubscribe subscriber/index.js
@@ -0,0 +1,14 @@
+var action = {
+    post: function(response) {
+        var result = app.isValid(response.Body, response.StatusCode);
+
+        if (result.isError) 
+        {
+            return app.returnError(result);
+        }
+        else
+        {
+            return success(result.body);
+        }
+    }
+};
